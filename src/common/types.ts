cc69823839ca465ff199497ab5c90ecb6708924d
David Gan <dgan979@gmail.com>
2023-12-20 22:48:36 -0700

    fixed up

--- src/common/types.ts
+++
@@ -1,6 +0,0 @@
-import { InstanceData, InstanceSchema } from '@manifoldxyz/studio-app-sdk';
-import { Step } from '@manifoldxyz/studio-app-sdk-react';
-import { schema } from '@/schema';
-
-export type SingleMintInstance = InstanceData<InstanceSchema<typeof schema.fields>>;
-export type SingleMintStep = Step<typeof schema.fields>;
